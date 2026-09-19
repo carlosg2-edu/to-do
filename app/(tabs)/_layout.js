f1dd68b07663c2ae61aa9c4740e719e2169931aa
carlosg2-edu <carlos.hernandezg2@uanl.edu.mx>
2026-09-18 19:52:28 -0600

    implementacion de pestañas

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -3,8 +3,8 @@ import { Tabs } from "expo-router";
 export default function TabsLayout(){
     return(
         <Tabs screenOptions={{headerShown: true}}>
-            <Tabs.Screen name="index"/>
-            <Tabs.Screen name="alumnos"/>
+            <Tabs.Screen name="index" options={{title: "Inicio"}}/>
+            <Tabs.Screen name="materias"/>
         </Tabs>
     )
 }
